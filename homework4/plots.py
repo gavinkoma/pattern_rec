660ad9595ba6b6bf998139e16ff55593a690b87f
Gavin Koma <gavintkoma@gmail.com>
2023-02-09 13:30:10 -0500

    still need graphs to be done

--- homework4/plots.py
+++ homework4/plots.py
@@ -23,20 +23,32 @@ for file in files: #loop through .csv names
     #there are no headers for any of this stuff, dont forget
     d_all["{0}".format(file)] = pd.read_csv(file,header=None) 
 
+name_dict = {
+    0: "data8dev.csv",
+    1: "data8eval.csv",
+    2: "data8train.csv",
+    3: "data8train.csv",
+    4: "data9dev.csv",
+    5: "data9eval.csv",
+    6: "data9train.csv",
+    7: "data10dev.csv",
+    8: "data10eval.csv",
+    9: "data10train.csv"
+}
+
 #%% plots
-def plots():
-    data_keys = list(d_all.keys())
-    data_val = list(d_all.values())
-    for kval in data_val:
-        plt.figure()
-        #print(kval[1],kval[2])
-        plt.scatter(kval[1],kval[2],c=kval[0],alpha=0.2)
-        plt.title(str(data_keys[0]))
-        plt.ylabel("y_vector")
-        plt.xlabel("x_vector")
-        plt.show()
-        
-plots()
+
+data_val = list(d_all.values())
+count = 0
+for kval in data_val:
+    plt.figure()
+    #print(kval[1],kval[2])
+    plt.scatter(kval[1],kval[2],c=kval[0],alpha=0.2)
+    plt.title(str(count))
+    plt.ylabel("y_vector")
+    plt.xlabel("x_vector")
+    plt.show()
+    count += 1
 
 
 
