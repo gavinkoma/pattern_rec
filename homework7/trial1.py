d66b845ebe29d812cf1d417d79029034b6ad33e5
Gavin Koma <gavintkoma@gmail.com>
2023-03-09 01:02:19 -0600

    added homework 7

--- homework7/trial1.py
+++ homework7/trial1.py
@@ -11,8 +11,12 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-os.chdir(r'/Users/gavinkoma/Desktop/pattern_rec/homework8/')
+os.chdir(r'/Users/gavinkoma/Desktop/pattern_rec/homework7/data')
 print(os.getcwd())
 
-dev = pd.read_csv('dev.csv')
-evaluation = pd.read_csv('eval.csv')
+dev = pd.read_csv('dev.csv',header=None)
+evaluation = pd.read_csv('eval.csv',header=None)
+
+
+print(len(dev))
+print(len(evaluation))
