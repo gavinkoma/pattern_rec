f8263e6eab5348c7fcba892b30ac54caa9d52ba1
Gavin Koma <gavintkoma@gmail.com>
2023-03-27 18:15:11 -0400

    added exam materials

---
+++ exam_redo/attempt_1.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#going to just place hold
+
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn import neighbors,datasets
+from sklearn.ensemble import RandomForestClassifier
+import numpy as np
+from matplotlib.colors import ListedColormap
+import seaborn as sns
+
+os.chdir("/Users/gavinkoma/Desktop/pattern_rec/exam_redo")
+
+#%%question 1a
+#okay lets recreate these data values
+values = pd.DataFrame([[0.0, 0.50, 0],
+                       [-0.75,0.00,0],
+                       [-0.25, 0.00, 0],
+                       [0.25,0.00,0],
+                       [0.75,0.00,0],
+                       [-0.50,0,1],
+                       [0.0,0.0,1],
+                       [0.50,0.0,1],
+                       [0,-0.50,1]])
+
+x0 = values.iloc[:,0]
+x1 = values.iloc[:,1]
+y = values.iloc[:,2]
+X = values.iloc[:,0:2]
+
+plt.scatter(values[0],values[1],c=values[2])
+plt.xlim(-1,1)
+plt.ylim(-1,1)
+
+n_neighbors = 3
+
+clf = neighbors.KNeighborsClassifier(n_neighbors,weights='uniform')
+clf.fit(X,y)
+
+cmap_light = ListedColormap(['orange', 'cyan', 'cornflowerblue'])
+cmap_bold = ['darkorange', 'c', 'darkblue']
+
+PAD = 1.0 # how much to "pad" around the min/max values. helps in setting bounds of plot
+
+x_min, x_max = X.iloc[:, 0].min() - 1, X.iloc[:, 0].max() + 1
+y_min, y_max = X.iloc[:, 1].min() - 1, X.iloc[:, 1].max() + 1
+
+h=0.2
+
+xx,yy = np.meshgrid(np.arange(x_min,x_max,h),
+                    np.arange(y_min,y_max,h))
+
+Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+Z = Z.reshape(xx.shape)
+
+plt.figure()
+plt.contourf(xx, yy, Z, cmap=cmap_light)
+
+sns.scatterplot(x=X.iloc[:, 0], y=X.iloc[:, 1],hue=y,
+                palette=cmap_bold, alpha=1.0, edgecolor="black")
+
+plt.xlim(xx.min(), xx.max())
+plt.ylim(yy.min(), yy.max())
+
+plt.title("3-Class classification (k = %i, 'uniform' = '%s')"
+% (n_neighbors, 'uniform'))
+
+plt.show()
+
+#%%question 1b
+#okay so we need to run a random forest and also plot the dec. boundary again
+#err maybe we dont need to plot the boundary and we can just verify values
+
+values = pd.DataFrame([[-0.50, 0.50, 0],
+                       [-0.50, -0.50, 0],
+                       [0.50, -0.50, 0],
+                       [0.50, 0.50, 0],
+                       [0.00, 0.00, 0],
+                       [-0.45,0.45,1],
+                       [-0.45,-0.45,1],
+                       [0.45,0.45,1],
+                       [0.45,-0.45,1]])
+
+plt.scatter(values[0],values[1],c=values[2])
+plt.xlim(-1,1)
+plt.ylim(-1,1)
+test_values = values.iloc[:,0:2]
+test_features = values.iloc[:,2]
+
+rf = RandomForestClassifier(n_estimators=3,random_state=45)
+                      
+rf.fit(values.iloc[:,0:1],values.iloc[:,2])
+y_pred = rf.predict(test_features)
+
+
+
---
+++ homework9/trial1.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 26 18:26:19 2023
+
+@author: gavinkoma
+"""
+
+import os
+import pandas as pd
+
+os.chdir("/Users/gavinkoma/Desktop/pattern_rec/homework9/data")
+
+
+#%% dataprep
+#use the entire training data set
+
+conventional = pd.read_csv("00_train_03.csv",header=None)
+
+
+
+
+
+
+
+
+
+
+
