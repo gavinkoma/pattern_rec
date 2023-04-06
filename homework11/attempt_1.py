976dd098289464e65a5a0d1b28ca84748003e018
Gavin Koma <gavintkoma@gmail.com>
2023-04-06 15:57:07 -0400

    i think i finished the feedforward cnn

--- homework11/attempt_1.py
+++ homework11/attempt_1.py
@@ -12,7 +12,8 @@ import pandas as pd
 import os
 import seaborn as sns
 import matplotlib.pyplot as plt
-
+import torch
+import torch.nn as nn
 
 #%%import data and do very basic graphs
 os.chdir("/Users/gavinkoma/Desktop/pattern_rec/homework11/data")
@@ -42,9 +43,106 @@ sns.scatterplot(x=eval_data.iloc[:,1],
 
 
 #%%implementation of SLP
+x = torch.ones(1,requires_grad=True)
+print(x.grad) #returns None initially
 
+y=x+2
+z=y*y*2
+z.backward()
+print(x.grad) #this is partial z/x
 
+class Feedforward(torch.nn.Module):
+    def __init__(self, input_size, hidden_size):
+        super(Feedforward, self).__init__()
+        self.input_size = input_size
+        self.hidden_size  = hidden_size
+        self.fc1 = torch.nn.Linear(self.input_size, 
+                                   self.hidden_size)
+        self.relu = torch.nn.ReLU()
+        self.fc2 = torch.nn.Linear(self.hidden_size, 1)
+        self.sigmoid = torch.nn.Sigmoid()
+        
+    def forward(self, x):
+        hidden = self.fc1(x)
+        relu = self.relu(hidden)
+        output = self.fc2(relu)
+        output = self.sigmoid(output)
+        return output
+    
+#%%prep the datasets for training
+x_train = train_data.iloc[:,1:3]
+y_train = train_data.iloc[:,0]
+x_dev = dev_data.iloc[:,1:3]
+y_dev = dev_data.iloc[:,0]
+x_eval = eval_data.iloc[:,1:3]
+y_eval = eval_data.iloc[:,0]
 
+x_train = torch.Tensor(x_train.values) #convert to an array 
+x_dev = torch.Tensor(x_dev.values)
+x_eval = torch.Tensor(x_eval.values) #array
+
+x_train = torch.FloatTensor(x_train)
+y_train = torch.FloatTensor(y_train)
+x_dev = torch.FloatTensor(x_dev)
+y_dev = torch.FloatTensor(y_dev)
+x_eval = torch.FloatTensor(x_eval)
+y_eval = torch.FloatTensor(y_eval)
+
+#%%implement the model w/ tensors
+model = Feedforward(2,200)
+criterion = torch.nn.MSELoss()
+optimizer = torch.optim.SGD(model.parameters(), 
+                            lr = 0.01)
+
+#%%train the model
+model.eval()
+y_pred = model(x_eval)
+before_train = criterion(y_pred.squeeze(),y_eval)
+print('test loss before training',before_train.item())
+
+model.train()
+epoch = 50
+
+epoch_val = []
+loss_val = []
+
+for epoch in range(epoch):
+    optimizer.zero_grad()
+    
+    #forward pass
+    y_pred = model(x_train)
+    
+    #loss computation
+    loss = criterion(y_pred.squeeze(),y_train)
+    epoch_val.append(epoch)
+    loss_val.append(loss.detach())
+    print('Epoch: {} \n Train Loss: {} \n'.format(epoch,loss.item()))
+    
+    #backward pass
+    loss.backward()
+    optimizer.step()
+
+plt.figure()
+sns.scatterplot(x = epoch_val,y=loss_val).set_title("Loss vs. Epoch #")
+plt.xlabel("Epoch #")
+plt.ylabel("Loss Value")
+
+#%%evaluation
+model.eval()
+y_pred = model(x_eval)
+after_train = criterion(y_pred.squeeze(),y_eval)
+print('Test loss after training: ',after_train.item())
+    
+#%%accuracy
+def accuracy(outputs, labels):
+    _, preds = torch.max(outputs, dim=1)
+    return torch.tensor(torch.sum(preds == labels).item() / len(preds))
+
+accuracy_train = accuracy(x_train,y_train)
+accuracy_eval = accuracy(y_pred,y_eval)
+
+print("Accuracy of Train: ", accuracy_train)
+print("Accuracy of Eval: ", accuracy_eval)
 
 
 
